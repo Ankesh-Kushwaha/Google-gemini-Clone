6917a7c8f3ab488821ef418b318fffd3da3b242a
ankeshkushwaha <ankeshkush9651@gmail.com>
2024-10-29 21:50:17 +0530

    complete the project

---
+++ Google-gemini/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SideBar from './components/SideBar/sideBar'
+import Main from  './components/main/main'
+const App = () => {
+  return <>
+    <SideBar />
+     <Main></Main>
+  </>
+}
+
+export default App
